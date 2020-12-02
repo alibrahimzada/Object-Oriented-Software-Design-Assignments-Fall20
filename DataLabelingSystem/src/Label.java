96ce585bab7f689c48d136e03fc97f49861d6673
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2020-12-02 17:11:24 +0300

    added label class

--- DataLabelingSystem/src/Label.java
+++ DataLabelingSystem/src/Label.java
@@ -1,4 +1,21 @@
+package playgroundPack;
 
 public class Label {
-    
+  private int id;
+  private String text;
+  
+  public Label(int id, String text) {
+    this.id = id;
+    this.text = text;
+
+  }
+
+  public int getId(){
+    return this.id;
+  }
+
+  public String getText(){
+    return this.text;    
+  }
+
 }

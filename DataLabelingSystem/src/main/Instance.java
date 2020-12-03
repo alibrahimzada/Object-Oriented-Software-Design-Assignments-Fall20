09eedacfd2216eaf56000f0eb22a37f191c0376c
Amela Karmaj <74592467+amelakj@users.noreply.github.com>
2020-12-03 22:49:20 +0300

    Update Instance.java

--- DataLabelingSystem/src/main/Instance.java
+++ DataLabelingSystem/src/main/Instance.java
@@ -1,5 +1,19 @@
 package main;
 
 public class Instance {
+    private int id;
+    private String text ;
     
+    public Instance(int id, String text){
+	this.id = id;
+	this.text = text;
+    }
+
+    public int getId() {
+    	return this.id;
+    }
+    
+    public String getText() {
+    	return this.text;
+    }    
 }

1d5d32ae2763f46498c961e124aea034be0237cc
alibrahimzada <alibrahimzada@gmail.com>
2020-12-03 10:28:38 +0300

    added main package

---
+++ DataLabelingSystem/src/main/DataLabelingSystem.java
@@ -0,0 +1,7 @@
+package main;
+
+public class DataLabelingSystem {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
---
+++ DataLabelingSystem/src/main/Label.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Label {
+  private int id;
+  private String text;
+  
+  public Label(int id, String text) {
+    this.id = id;
+    this.text = text;
+
+  }
+
+  public int getId(){
+    return this.id;
+  }
+
+  public String getText(){
+    return this.text;    
+  }
+
+}

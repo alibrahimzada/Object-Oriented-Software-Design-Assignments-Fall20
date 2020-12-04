cf6746609e43a4fe2a1c50b44d6a06209bbf4ac1
alibrahimzada <alibrahimzada@gmail.com>
2020-12-04 13:36:33 +0300

    updated Log class

--- DataLabelingSystem/Iteration 1/src/main/Log.java
+++ DataLabelingSystem/Iteration 1/src/main/Log.java
@@ -8,28 +8,28 @@ import java.util.logging.SimpleFormatter;
 public class Log {
     private Logger logger;
     private FileHandler fileHandler;
-    private String filename = "DataLabelingSystem.log";
+    private String fileName = "DataLabelingSystem.log";
     private String loggerName = "DataLabelingSystemLogger";
 
     public Log() throws SecurityException, IOException {
-        File file = new File(filename);
+        File file = new File(this.fileName);
 
         if (!file.exists()) {
             file.createNewFile(); // creating a file with the above name if it does not exist.
         }
 
-        fileHandler = new FileHandler(filename, true); // append=true, to avoid overwriting. 
-        logger = Logger.getLogger(loggerName);// creating a logger with name: DataLabelingSystemLogger.
+        fileHandler = new FileHandler(this.fileName, true); // append=true, to avoid overwriting.
+        logger = Logger.getLogger(this.loggerName);// creating a logger with name: DataLabelingSystemLogger.
         logger.addHandler(fileHandler);
         SimpleFormatter formatter = new SimpleFormatter(); 
         fileHandler.setFormatter(formatter); // Print a brief summary of the LogRecord in a readable format.
     }
 
     public String getFileName(){
-        return filename;
+        return this.fileName;
     }
 
     public Logger getLogger() {
-        return logger;
+        return this.logger;
     }
 }

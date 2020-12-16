4c47abd91108c8bb40afaf0e907c70a5b97ce721
alibrahimzada <alibrahimzada@gmail.com>
2020-12-16 16:29:08 +0300

    added raw methods for User class

--- DataLabelingSystem/Iteration_2/src/main/User.java
+++ DataLabelingSystem/Iteration_2/src/main/User.java
@@ -1,14 +1,30 @@
 package main;
 
+import java.util.ArrayList;
+
 public class User {
     private int id;
     private String name;
     private String type;
+    private ArrayList<Dataset> assignedDatasets;
+    private ArrayList<LabelAssignment> labelAssignments;
+    private double timeSpent;
 
     public User(int id, String name, String type) {
         this.id = id;
         this.name = name;
         this.type = type;
+        this.assignedDatasets = new ArrayList<Dataset>();
+        this.labelAssignments = new ArrayList<LabelAssignment>();
+        this.timeSpent = 0;
+    }
+
+    public void addAssignedDataset(Dataset dataset) {
+        this.assignedDatasets.add(dataset);
+    }
+
+    public void addLabelAssignment(LabelAssignment labelAssignment) {
+        this.labelAssignments.add(labelAssignment);
     }
 
     public int getId() {
@@ -21,5 +37,33 @@ public class User {
 
     public String getType() {
         return this.type;
+    }
+
+    public int getTotalDatasets() {
+        return this.assignedDatasets.size();
+    }
+
+    public String getCompletenessPercentage(Dataset dataset) {
+        return "";
+    }
+
+    public int getTotalLabelings() {
+        return 1;
+    }
+
+    public int getUniqueLabelings() {
+        return 1;
+    }
+
+    public String getConsistencyPercentage() {
+        return "";
+    }
+
+    public double getAverageTime() {
+        return 1.0;
+    }
+
+    public double getStdDevTime() {
+        return 1.0;
     }
 }

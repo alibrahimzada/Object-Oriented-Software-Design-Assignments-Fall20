3d269b0caaffd0c811b7bf8752f121fb870b20d4
alibrahimzada <alibrahimzada@gmail.com>
2020-12-16 16:11:31 +0300

    added getters for Instance class

--- DataLabelingSystem/Iteration_2/src/main/Instance.java
+++ DataLabelingSystem/Iteration_2/src/main/Instance.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Instance {
     private int id;
-    private String text ;
+    private String text;
+    private Dataset dataset;
+    private ArrayList<LabelAssignment> labelAssignments;
     
     public Instance(int id, String text){
         this.id = id;
@@ -15,5 +19,29 @@ public class Instance {
     
     public String getText() {
     	return this.text;
-    }    
+    }
+
+    public Dataset getDataset() {
+        return this.dataset;
+    }
+    
+    public int getTotalAssignments() {
+        return this.labelAssignments.size();
+    }
+
+    public int getUniqueAssignments() {
+        return 1;
+    }
+
+    public int getUniqueUsers() {
+        return 1;
+    }
+
+    public String getFrequentLabel() {
+        return "";
+    }
+
+    public double getEntropy() {
+        return 1.0;
+    }
 }

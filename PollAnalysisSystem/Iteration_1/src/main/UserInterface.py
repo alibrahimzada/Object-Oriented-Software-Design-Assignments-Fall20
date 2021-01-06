103765198112e4c7cabd42a04166196f92d209ee
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2021-01-06 15:11:52 +0300

    cleaned repeatativness

--- PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
+++ PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
@@ -9,53 +9,44 @@ class UserInterface(object):
         UserInterface.setObjectName("UserInterface")
         UserInterface.resize(748, 491)
         UserInterface.setStyleSheet("background-color:white")
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
         self.centralwidget = QtWidgets.QWidget(UserInterface)
         self.centralwidget.setObjectName("centralwidget")
         self.TitleLabel = QtWidgets.QLabel(self.centralwidget)
         self.TitleLabel.setGeometry(QtCore.QRect(0, 0, 751, 81))
         self.LoadStdListsBtn = QtWidgets.QPushButton(self.centralwidget)
         self.LoadStdListsBtn.setGeometry(QtCore.QRect(60, 150, 171, 50))
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
+       
         self.LoadStdListsBtn.setFont(font)
         self.LoadStdListsBtn.setStyleSheet("background-color:green; color:white")
         self.LoadStdListsBtn.setObjectName("LoadStdListsBtn")
         self.LoadPollsBtn = QtWidgets.QPushButton(self.centralwidget)
         self.LoadPollsBtn.setGeometry(QtCore.QRect(290, 150, 171, 50))
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
+
         self.LoadPollsBtn.setFont(font)
         self.LoadPollsBtn.setObjectName("LoadPollsBtn")
         self.LoadAnswerKeysBtn = QtWidgets.QPushButton(self.centralwidget)
         self.LoadAnswerKeysBtn.setGeometry(QtCore.QRect(520, 150, 171, 50))
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
+  
         self.LoadAnswerKeysBtn.setFont(font)
         self.LoadAnswerKeysBtn.setObjectName("LoadAnswerKeysBtn")
 
         self.ExportAttendReportBtn = QtWidgets.QPushButton(self.centralwidget)
         self.ExportAttendReportBtn.setGeometry(QtCore.QRect(60, 310, 171, 51))
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
+   
         self.ExportAttendReportBtn.setFont(font)
         self.ExportAttendReportBtn.setObjectName("ExportAttendReportBtn")
         self.ExportStatsBtn = QtWidgets.QPushButton(self.centralwidget)
         self.ExportStatsBtn.setGeometry(QtCore.QRect(290, 310, 171, 51))
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
+    
         self.ExportStatsBtn.setFont(font)
         self.ExportStatsBtn.setStyleSheet("background-color:green; color:white;")
         self.ExportStatsBtn.setObjectName("ExportStatsBtn")
         self.ExportGlobalReportBtn = QtWidgets.QPushButton(self.centralwidget)
         self.ExportGlobalReportBtn.setGeometry(QtCore.QRect(520, 310, 171, 51))
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
+    
         self.ExportGlobalReportBtn.setFont(font)
         self.ExportGlobalReportBtn.setAutoDefault(False)
         self.ExportGlobalReportBtn.setDefault(False)
@@ -66,9 +57,13 @@ class UserInterface(object):
 
         self.retranslateUi(UserInterface)
         QtCore.QMetaObject.connectSlotsByName(UserInterface)
-
-
   def retranslateUi(self, UserInterface):
+    ''' 
+        This is somewhat self explantory, just a function that sets the text/style of the widgets we have.
+        This is how we do it in pyqt but we can always get rid of this function and just write whatever we have
+        here into the setup ui function but this is the correct way to do it.
+    '''
+
       _translate = QtCore.QCoreApplication.translate
       UserInterface.setWindowTitle(_translate("UserInterface", "MainWindow"))
       self.LoadStdListsBtn.setText(_translate("UserInterface", "Load Student Lists"))
@@ -85,10 +80,10 @@ class UserInterface(object):
       self.TitleLabel.setText(_translate("UserInterface", "Poll Analysis System"))
 
 def main():
-    app = QApplication([])
-    window = QMainWindow()
+    app = QApplication([]) #Qapplication requires that we pass it system arguments, but since we have none, we just put an empty list.
+    window = QMainWindow() #the main window to which we pass our widgets
     ui = UserInterface()
     ui.setupUi(window)
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) #app.exec is what shows our window basically, without it, we won't see anything. 
 

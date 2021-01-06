a7b4c08146ccb94e5430e4f7cb100f1633807ca2
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2021-01-06 22:12:51 +0300

    1. Cleaned some code that was not needed. 2. Fixed issue of window resizing. 3. Fixed buttons issue collapsing. 4. Added pointing hand cursor on button hover. 5. Switched buttons of load polls/load answer keys.

--- PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
+++ PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
@@ -1,13 +1,15 @@
 
 
 import sys
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QCursor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class UserInterface(object):
   def setupUi(self, UserInterface):
         UserInterface.setObjectName("UserInterface")
-        UserInterface.resize(748, 491)
+        UserInterface.resize(730, 461)
         UserInterface.setStyleSheet("background-color:white")
         font = QtGui.QFont()
         font.setBold(True)
@@ -17,43 +19,46 @@ class UserInterface(object):
         self.TitleLabel = QtWidgets.QLabel(self.centralwidget)
         self.TitleLabel.setGeometry(QtCore.QRect(0, 0, 751, 81))
         self.LoadStdListsBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.LoadStdListsBtn.setGeometry(QtCore.QRect(60, 150, 171, 50))
+        self.LoadStdListsBtn.setGeometry(QtCore.QRect(30, 150, 200, 70))
+        self.LoadStdListsBtn.setMinimumSize(QtCore.QSize(200, 70))
        
         self.LoadStdListsBtn.setFont(font)
         self.LoadStdListsBtn.setStyleSheet("background-color:green; color:white")
         self.LoadStdListsBtn.setObjectName("LoadStdListsBtn")
-        self.LoadPollsBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.LoadPollsBtn.setGeometry(QtCore.QRect(290, 150, 171, 50))
 
-        self.LoadPollsBtn.setFont(font)
-        self.LoadPollsBtn.setObjectName("LoadPollsBtn")
-        self.LoadAnswerKeysBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.LoadAnswerKeysBtn.setGeometry(QtCore.QRect(520, 150, 171, 50))
-  
-        self.LoadAnswerKeysBtn.setFont(font)
-        self.LoadAnswerKeysBtn.setObjectName("LoadAnswerKeysBtn")
+        self.LoadAnswerKey = QtWidgets.QPushButton(self.centralwidget)
+        self.LoadAnswerKey.setGeometry(QtCore.QRect(260, 150, 200, 70))
+        self.LoadAnswerKey.setMinimumSize(QtCore.QSize(200, 70))
+        self.LoadAnswerKey.setFont(font)
+        self.LoadAnswerKey.setObjectName("LoadAnswerKey")
+
+        self.LoadPolls = QtWidgets.QPushButton(self.centralwidget)
+        self.LoadPolls.setGeometry(QtCore.QRect(490, 150, 200, 70))
+        self.LoadPolls.setMinimumSize(QtCore.QSize(200, 70))
+        self.LoadPolls.setFont(font)
+        self.LoadPolls.setObjectName("LoadPolls")
 
         self.ExportAttendReportBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.ExportAttendReportBtn.setGeometry(QtCore.QRect(60, 310, 171, 51))
-   
+        self.ExportAttendReportBtn.setGeometry(QtCore.QRect(30, 310, 200, 70))
+        self.ExportAttendReportBtn.setMinimumSize(QtCore.QSize(200, 70))
         self.ExportAttendReportBtn.setFont(font)
         self.ExportAttendReportBtn.setObjectName("ExportAttendReportBtn")
+        
+
         self.ExportStatsBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.ExportStatsBtn.setGeometry(QtCore.QRect(290, 310, 171, 51))
-    
+        self.ExportStatsBtn.setGeometry(QtCore.QRect(260, 310, 200, 70))
+        self.ExportStatsBtn.setMinimumSize(QtCore.QSize(200, 70))
         self.ExportStatsBtn.setFont(font)
         self.ExportStatsBtn.setStyleSheet("background-color:green; color:white;")
         self.ExportStatsBtn.setObjectName("ExportStatsBtn")
+
         self.ExportGlobalReportBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.ExportGlobalReportBtn.setGeometry(QtCore.QRect(520, 310, 171, 51))
-    
+        self.ExportGlobalReportBtn.setGeometry(QtCore.QRect(490, 310, 200, 70))
+        self.ExportGlobalReportBtn.setMinimumSize(QtCore.QSize(200, 70))
         self.ExportGlobalReportBtn.setFont(font)
-        self.ExportGlobalReportBtn.setAutoDefault(False)
-        self.ExportGlobalReportBtn.setDefault(False)
-        self.ExportGlobalReportBtn.setFlat(False)
         self.ExportGlobalReportBtn.setObjectName("ExportGlobalReportBtn")
 
-
+        ## CUSTOMIZATION, you probably won't change anything here so please don't change unless you ask me first.
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -65,12 +70,9 @@ class UserInterface(object):
         font.setFamily("MS Sans Serif")
         font.setPointSize(35)
         font.setBold(True)
-        font.setUnderline(False)
         font.setWeight(75)
-        font.setStrikeOut(False)
         font.setKerning(True)
         self.TitleLabel.setFont(font)
-        self.TitleLabel.setAutoFillBackground(False)
         self.TitleLabel.setStyleSheet("background-color: green; color: white")
         self.TitleLabel.setLineWidth(20)
         self.TitleLabel.setMidLineWidth(20)
@@ -80,6 +82,14 @@ class UserInterface(object):
         self.statusbar = QtWidgets.QStatusBar(UserInterface)
         self.statusbar.setObjectName("statusbar")
         UserInterface.setStatusBar(self.statusbar)
+# your_button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        self.LoadAnswerKey.setCursor(QtCore.Qt.PointingHandCursor)
+        self.LoadPolls.setCursor(QtCore.Qt.PointingHandCursor)
+        self.LoadStdListsBtn.setCursor(QtCore.Qt.PointingHandCursor)
+        self.ExportAttendReportBtn.setCursor(QtCore.Qt.PointingHandCursor)
+        self.ExportGlobalReportBtn.setCursor(QtCore.Qt.PointingHandCursor)
+        self.ExportStatsBtn.setCursor(QtCore.Qt.PointingHandCursor)
+
 
         self.retranslateUi(UserInterface)
         QtCore.QMetaObject.connectSlotsByName(UserInterface)
@@ -92,28 +102,34 @@ class UserInterface(object):
     '''
     _translate = QtCore.QCoreApplication.translate
     UserInterface.setWindowTitle(_translate("UserInterface", "MainWindow"))
-    self.LoadStdListsBtn.setText(_translate("UserInterface", "Load Student Lists"))
-    self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is just a button, why are you hovering over many things weirdly?"))
-    self.LoadPollsBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.LoadPollsBtn.setText(_translate("UserInterface", "Load Polls"))
-    self.LoadAnswerKeysBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.LoadAnswerKeysBtn.setText(_translate("UserInterface", "Load Answer Keys"))
-    self.ExportAttendReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.ExportAttendReportBtn.setText(_translate("UserInterface", "Export Attendance Report"))
-    self.ExportStatsBtn.setText(_translate("UserInterface", "Export Stats"))
-    self.ExportGlobalReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.ExportGlobalReportBtn.setText(_translate("UserInterface", "Export Global Report"))
-    self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is also just a button, come on"))
-    self.TitleLabel.setStatusTip(_translate("UserInterface", "This is a normal label, nothing to see here."))
     self.TitleLabel.setText(_translate("UserInterface", "Poll Analysis System"))
 
+    self.LoadStdListsBtn.setText(_translate("UserInterface", "Load Student Lists"))
+    self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is just a button, why are you hovering over many things weirdly?"))
+    self.LoadAnswerKey.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+    self.LoadAnswerKey.setText(_translate("UserInterface", "Load Answer Keys"))
 
+    self.LoadPolls.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+    self.LoadPolls.setText(_translate("UserInterface", "Load Polls"))
+
+    self.ExportAttendReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+    self.ExportAttendReportBtn.setText(_translate("UserInterface", "Export Attendance Report"))
+
+    self.ExportStatsBtn.setText(_translate("UserInterface", "Export Stats"))
+
+    self.ExportGlobalReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+    self.ExportGlobalReportBtn.setText(_translate("UserInterface", "Export Global Report"))
+
+    self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is also just a button, come on"))
+    self.TitleLabel.setStatusTip(_translate("UserInterface", "This is a normal label, nothing to see here."))
 
 
 
 def main():
     app = QApplication([]) #Qapplication requires that we pass it system arguments, but since we have none, we just put an empty list.
+    app.setStyle("Breeze")
     window = QMainWindow() #the main window to which we pass our widgets
+    window.setFixedSize(730, 461)
     ui = UserInterface()
     ui.setupUi(window)
     window.show()

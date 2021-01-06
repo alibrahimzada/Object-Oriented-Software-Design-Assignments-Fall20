76aba4771ec9c70fd09935d9c473ba024da5b0c7
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2021-01-06 14:34:36 +0300

    top label, window size, top buttons shapes (without text)  and more

--- PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
+++ PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
@@ -5,7 +5,36 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class UserInterface(object):
-  pass
+  def setupUi(self, UserInterface):
+        UserInterface.setObjectName("UserInterface")
+        UserInterface.resize(748, 491)
+        UserInterface.setStyleSheet("background-color:white")
+        self.centralwidget = QtWidgets.QWidget(UserInterface)
+        self.centralwidget.setObjectName("centralwidget")
+        self.TitleLabel = QtWidgets.QLabel(self.centralwidget)
+        self.TitleLabel.setGeometry(QtCore.QRect(0, 0, 751, 81))
+        self.LoadStdListsBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.LoadStdListsBtn.setGeometry(QtCore.QRect(60, 150, 171, 50))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.LoadStdListsBtn.setFont(font)
+        self.LoadStdListsBtn.setStyleSheet("background-color:green; color:white")
+        self.LoadStdListsBtn.setObjectName("LoadStdListsBtn")
+        self.LoadPollsBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.LoadPollsBtn.setGeometry(QtCore.QRect(290, 150, 171, 50))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.LoadPollsBtn.setFont(font)
+        self.LoadPollsBtn.setObjectName("LoadPollsBtn")
+        self.LoadAnswerKeysBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.LoadAnswerKeysBtn.setGeometry(QtCore.QRect(520, 150, 171, 50))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.LoadAnswerKeysBtn.setFont(font)
+        self.LoadAnswerKeysBtn.setObjectName("LoadAnswerKeysBtn")
 
 
 

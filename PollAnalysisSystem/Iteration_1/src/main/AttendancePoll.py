7fec951066a369f243fe9735eb647cc1bd9d5052
Haytham <haythamabdelnabi@std.sehir.edu.tr>
2021-01-07 22:33:03 +0300

    AttendancePoll class skeleton

---
+++ PollAnalysisSystem/Iteration_1/src/main/AttendancePoll.py
@@ -0,0 +1,32 @@
+class AttendancePoll:
+    def __init__(self, name, day, date):
+        self.__name = name
+        self.__day = day
+        self.__date = date
+
+    # Name attribute
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    # Day attribute
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, value):
+        self.__day = value
+
+    # Date attribute
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value):
+        self.__date = value

0c4a5c3b3ab85c628b0b2bd9eec1c2457ecc64c3
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2021-01-07 13:43:21 +0300

    1. Added Skeleton for button bindings/events, 2. Added comments, 3. implemented file Dialog open/ saving path

--- PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
+++ PollAnalysisSystem/Iteration_1/src/main/UserInterface.py
@@ -3,10 +3,11 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
 
 class UserInterface(object):
-  def setupUi(self, UserInterface):
+
+    def setupUi(self, UserInterface):
         UserInterface.setObjectName("UserInterface")
         UserInterface.resize(730, 461)
         UserInterface.setStyleSheet("background-color:white")
@@ -20,7 +21,6 @@ class UserInterface(object):
         self.LoadStdListsBtn = QtWidgets.QPushButton(self.centralwidget)
         self.LoadStdListsBtn.setGeometry(QtCore.QRect(30, 150, 200, 70))
         self.LoadStdListsBtn.setMinimumSize(QtCore.QSize(200, 70))
-       
         self.LoadStdListsBtn.setFont(font)
         self.LoadStdListsBtn.setStyleSheet("background-color:green; color:white")
         self.LoadStdListsBtn.setObjectName("LoadStdListsBtn")
@@ -88,38 +88,86 @@ class UserInterface(object):
         self.ExportStatsBtn.setCursor(QtCore.Qt.PointingHandCursor)
 
 
+        ##### MAPPING:
+        self.LoadStdListsBtn.clicked.connect(self.uploadStdList)
+        self.LoadAnswerKey.clicked.connect(self.uploadAnswerKey)
+        self.LoadPolls.clicked.connect(self.uploadPolls)
+        self.ExportAttendReportBtn.clicked.connect(self.downloadAttendanceReport)
+        self.ExportStatsBtn.clicked.connect(self.downloadStats)
+        self.ExportGlobalReportBtn.clicked.connect(self.downloadGlobalReport)
+
+
         self.retranslateUi(UserInterface)
         QtCore.QMetaObject.connectSlotsByName(UserInterface)
 
-  def retranslateUi(self, UserInterface):
-    ''' 
+
+    def retranslateUi(self, UserInterface):
+        ''' 
         This is somewhat self explantory, just a function that sets the text/style of the widgets we have.
         This is how we do it in pyqt but we can always get rid of this function and just write whatever we have
         here into the setup ui function but this is the correct way to do it.
-    '''
-    _translate = QtCore.QCoreApplication.translate
-    UserInterface.setWindowTitle(_translate("UserInterface", "MainWindow"))
-    
-    self.TitleLabel.setText(_translate("UserInterface", "Poll Analysis System"))
+        '''
+        _translate = QtCore.QCoreApplication.translate
+        UserInterface.setWindowTitle(_translate("UserInterface", "MainWindow"))
+        
+        self.TitleLabel.setText(_translate("UserInterface", "Poll Analysis System"))
 
-    self.LoadStdListsBtn.setText(_translate("UserInterface", "Load Student Lists"))
-    self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is just a button, why are you hovering over many things weirdly?"))
-    self.LoadAnswerKey.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.LoadAnswerKey.setText(_translate("UserInterface", "Load Answer Keys"))
+        self.LoadStdListsBtn.setText(_translate("UserInterface", "Load Student Lists"))
+        self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is just a button, why are you hovering over many things weirdly?"))
+        self.LoadAnswerKey.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+        self.LoadAnswerKey.setText(_translate("UserInterface", "Load Answer Keys"))
 
-    self.LoadPolls.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.LoadPolls.setText(_translate("UserInterface", "Load Polls"))
+        self.LoadPolls.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+        self.LoadPolls.setText(_translate("UserInterface", "Load Polls"))
 
-    self.ExportAttendReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.ExportAttendReportBtn.setText(_translate("UserInterface", "Export Attendance Report"))
+        self.ExportAttendReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+        self.ExportAttendReportBtn.setText(_translate("UserInterface", "Export Attendance Report"))
 
-    self.ExportStatsBtn.setText(_translate("UserInterface", "Export Stats"))
+        self.ExportStatsBtn.setText(_translate("UserInterface", "Export Stats"))
 
-    self.ExportGlobalReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
-    self.ExportGlobalReportBtn.setText(_translate("UserInterface", "Export Global Report"))
+        self.ExportGlobalReportBtn.setStyleSheet(_translate("UserInterface", "background-color:green; color:white"))
+        self.ExportGlobalReportBtn.setText(_translate("UserInterface", "Export Global Report"))
 
-    self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is also just a button, come on"))
-    self.TitleLabel.setStatusTip(_translate("UserInterface", "This is a normal label, nothing to see here."))
+        self.LoadStdListsBtn.setStatusTip(_translate("UserInterface","This is also just a button, come on"))
+        self.TitleLabel.setStatusTip(_translate("UserInterface", "This is a normal label, nothing to see here."))
+
+
+    ############# BINDINGS
+    # READ ME: I feel liks this is somewhat repatative so I made a function that opens a dialog for all buttons and then we just return the path of the file opened to the function that was triggered. 
+    # However, we can still make this better but not sure how so will leave it for now.
+    def choseFileDialog(self, DialogName="Choose File"):
+        filename = QFileDialog.getOpenFileName(caption=DialogName) #Did not add a file type because not sure what type we are always getting
+        path = filename[0] 
+        #the function returns a tuple, the first value of the tuple is the path and the second value is the type of files you are allowing to choose.
+        return path
+
+    def uploadStdList(self):
+        path = self.choseFileDialog()
+        print(path)
+
+        # with open(path, "r") as StdList:
+            # print("bla")
+
+        
+
+    def uploadAnswerKey(self):
+        path = self.choseFileDialog()
+
+    def uploadPolls(self):
+        path = self.choseFileDialog()
+
+    def downloadAttendanceReport(self):
+        path = self.choseFileDialog()
+
+    def downloadGlobalReport(self):
+        path = self.choseFileDialog()
+
+    def downloadStats(self):
+        path = self.choseFileDialog()
+
+
+
+
 
 
 

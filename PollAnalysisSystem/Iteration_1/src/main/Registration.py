bcee4d99fb7f8fbb04b6063baefbcefb3cf19828
dean-pearce <a.asaad@windowslive.com>
2021-01-07 14:12:03 +0300

    Added Registration class

---
+++ PollAnalysisSystem/Iteration_1/src/main/Registration.py
@@ -0,0 +1,7 @@
+class Registration:
+    def __init__(self, academic_year, academic_semester, student, course, instructor):
+        self.academic_year = academic_year
+        self.academic_semester = academic_semester
+        self.student = student
+        self.course = course
+        self.instructor = instructor

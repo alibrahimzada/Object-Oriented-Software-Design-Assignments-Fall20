8078284a09bf5f1d47e7f55a870dbefc949dd0bf
dean-pearce <a.asaad@windowslive.com>
2021-01-09 17:57:44 +0300

    Fixed indentation error

--- PollAnalysisSystem/Iteration_1/src/main/Poll.py
+++ PollAnalysisSystem/Iteration_1/src/main/Poll.py
@@ -1,30 +1,27 @@
-
-
-
 class Poll(object):
-	def __init__(self, name, date, day):
-		self.__name = name
-		self.__date = date
-		self.__day = day
-		self.__poll_submissions = []
+    def __init__(self, name, date, day):
+        self.__name = name
+        self.__date = date
+        self.__day = day
+        self.__poll_submissions = []
 
-	@property
+    @property
     def name(self):
         return self.__name
-    
+
     @name.setter
     def name(self, value):
-        self.__name = value
-
-	@property
+      self.__name = value
+    
+    @property
     def date(self):
         return self.__date
     
     @date.setter
     def date(self, value):
         self.__date = value
-
-	@property
+    
+    @property
     def day(self):
         return self.__day
     
--- PollAnalysisSystem/Iteration_1/src/main/PollSubmission.py
+++ PollAnalysisSystem/Iteration_1/src/main/PollSubmission.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 class PollSubmission(object):
     def __init__(self, date, poll, student, questions, answers):
         self.__date = datetime.strptime(date, '%b %d, %Y %H:%M:%S')
@@ -7,7 +8,6 @@ class PollSubmission(object):
         self.__student = student
         self.__questions = questions
         self.__answers = answers
-
 
     # Date attribute
     @property

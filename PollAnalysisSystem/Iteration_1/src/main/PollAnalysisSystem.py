82e1c97f0fe3c4523b08b438ec178c45fc376a85
Asim Okby <asemokbey@gmail.com>
2021-01-13 12:52:35 +0300

    created a logger

--- PollAnalysisSystem/Iteration_1/src/main/PollAnalysisSystem.py
+++ PollAnalysisSystem/Iteration_1/src/main/PollAnalysisSystem.py
@@ -1,3 +1,4 @@
+import logging
 from tests import TestSuitRunner
 from main.AttendanceReportSerializer import AttendanceReportSerializer
 from main.GlobalReportSerializer import GlobalReportSerializer
@@ -16,6 +17,8 @@ class PollAnalysisSystem(object):
 		self.__attendance_report_serializer = AttendanceReportSerializer(self)
 		self.__global_report_serializer = GlobalReportSerializer(self)
 		self.__stats_report_serializer = StatsReportSerializer(self)
+		self.__logger = self.create_logger()
+		
 
 	@property
 	def answer_key_parser(self):
@@ -28,21 +31,31 @@ class PollAnalysisSystem(object):
 	@property
 	def poll_parser(self):
 		return self.__poll_parser
-	
+
+	@property
+	def logger(self):
+		return self.__logger
+
+	def create_logger(self):
+		logging.basicConfig(level=logging.NOTSET) # set root logger to NOSET & write to stdout
+		return logging.getLogger()
+		
 	def load_student_list(self, student_list_files):
 		self.__student_list_parser.parse_student_list(student_list_files)
+		self.__logger.info('The Student Lists were loaded successfully.')
 
 	def load_answer_key(self, answer_key_files):
 		self.__answer_key_parser.read_answer_keys(answer_key_files)
+		self.__logger.info('The Answer Keys were loaded successfully.')
 
 	def load_polls(self, poll_reports_files):
 		self.__poll_parser.read_poll_reports(poll_reports_files)
+		self.__logger.info('The Poll Reports were loaded successfully.')
 
 	def export_attendance(self):
 		pass
 
 	def export_statistics(self):
 		self.__stats_report_serializer.export_reports()
-
 	def export_global_report(self):
 		pass

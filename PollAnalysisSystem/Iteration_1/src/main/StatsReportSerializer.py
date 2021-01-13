4dd227beb4540b8fac33aa7d285b0fa18c2f8270
alibrahimzada <alibrahimzada@gmail.com>
2021-01-13 12:34:37 +0300

    updated access modifiers

--- PollAnalysisSystem/Iteration_1/src/main/StatsReportSerializer.py
+++ PollAnalysisSystem/Iteration_1/src/main/StatsReportSerializer.py
@@ -14,18 +14,14 @@ class StatsReportSerializer(object):
 		self.__answer_key_parser = poll_analysis_system.answer_key_parser
 		self.__global_df = pd.DataFrame()
 
-	@property
-	def poll_analysis_system(self):
-		return self.__poll_analysis_system
-
 	def export_reports(self):
 		for poll_name in self.__poll_parser.polls:
 			poll = self.__poll_parser.polls[poll_name]
 			if isinstance(poll, AttendancePoll): continue
-			self.export_quiz_report(poll_name, poll)
-		self.export_global_report()
+			self.__export_quiz_report(poll_name, poll)
+		self.__export_global_report()
 
-	def export_quiz_report(self, poll_name, poll):
+	def __export_quiz_report(self, poll_name, poll):
 		if not os.path.exists('statistics'):
 			os.mkdir('statistics')
 		os.chdir('statistics')
@@ -49,13 +45,13 @@ class StatsReportSerializer(object):
 				poll_submissions = registration.student.poll_submissions
 
 				if poll_name not in poll_submissions:
-					self.export_absent_student()
+					self.__export_absent_student()
 					continue
 				for poll_submission in poll_submissions[poll_name]:
-					self.validate_answers(poll_name, poll_submission)
-					self.update_answer_distribution(poll_submission)
+					self.__validate_answers(poll_name, poll_submission)
+					self.__update_answer_distribution(poll_submission)
 
-		self.create_chart()
+		self.__create_chart()
 
 		quiz_df = pd.DataFrame()
 		quiz_df['Student ID'] = student_numbers
@@ -76,9 +72,9 @@ class StatsReportSerializer(object):
 		quiz_df.to_excel('quiz_report.xlsx', index=False)
 		os.chdir('..')
 		os.chdir('..')
-		self.add_global_report(poll_name, poll, student_info)
+		self.__add_global_report(poll_name, poll, student_info)
 
-	def add_global_report(self, poll_name, poll, student_info):
+	def __add_global_report(self, poll_name, poll, student_info):
 		for column in student_info:
 			if column in self.__global_df: continue
 			self.__global_df[column] = student_info[column]
@@ -90,14 +86,14 @@ class StatsReportSerializer(object):
 		if poll_name + ' success %' not in self.__global_df:
 			self.__global_df[poll_name + ' success %'] = self.__quiz_questions['success %']
 
-	def export_global_report(self):
+	def __export_global_report(self):
 		if not os.path.exists('global_report'):
 			os.mkdir('global_report')
 		os.chdir('global_report')
 		self.__global_df.to_excel('global_report.xlsx', index=False)
 		os.chdir('..')
 
-	def export_absent_student(self):
+	def __export_absent_student(self):
 		self.__quiz_questions['n questions'].append(0)
 		self.__quiz_questions['success rate'].append('0/0')
 		self.__quiz_questions['success %'].append('0%')
@@ -106,7 +102,7 @@ class StatsReportSerializer(object):
 			if question_text not in ['n questions', 'success rate', 'success %']:
 				self.__quiz_questions[question_text].append('absent')
 	
-	def validate_answers(self, poll_name, poll_submission):
+	def __validate_answers(self, poll_name, poll_submission):
 		self.__quiz_questions['n questions'].append(0)
 		self.__quiz_questions['success rate'].append('0/0')
 		self.__quiz_questions['success %'].append('')
@@ -126,22 +122,22 @@ class StatsReportSerializer(object):
 			self.__quiz_questions['success rate'][-1] = '{}/{}'.format(total_correct, self.__quiz_questions['n questions'][-1])
 			self.__quiz_questions['success %'][-1] = '{}%'.format(round(total_correct * 100.0 / self.__quiz_questions['n questions'][-1], 2))
 
-	def update_answer_distribution(self, poll_submission):
+	def __update_answer_distribution(self, poll_submission):
 		for question in poll_submission.questions_answers:
 			for answer in poll_submission.questions_answers[question]:
 				self.__answer_distribution[question].setdefault(answer, 0)
 				self.__answer_distribution[question][answer] += 1
 
-	def create_chart(self):
+	def __create_chart(self):
 		q_counter = 1
 		for question in self.__answer_distribution:
 			if question.is_multiple_choice:
-				self.create_bar_chart(question, q_counter)
+				self.__create_bar_chart(question, q_counter)
 			else:
-				self.create_pie_chart(question, q_counter)
+				self.__create_pie_chart(question, q_counter)
 			q_counter += 1
 
-	def create_pie_chart(self, question, q_counter):
+	def __create_pie_chart(self, question, q_counter):
 		labels = []
 		sizes = []
 		explode = []
@@ -159,7 +155,7 @@ class StatsReportSerializer(object):
 		ax1.set_title(question.text, pad=20, fontfamily='serif')
 		plt.savefig('Q' + str(q_counter) + '.png', bbox_inches='tight')
 
-	def create_bar_chart(self, question, q_counter):
+	def __create_bar_chart(self, question, q_counter):
 		fig = plt.figure()
 		colors = []
 		sizes = []
